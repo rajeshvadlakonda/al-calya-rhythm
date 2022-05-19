af814d1351ef27875aa6e94903d8def1326af36b
rajeshvadlakonda <rajesh.vadlakonda101@gmail.com>
2022-05-19 17:55:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -9,14 +9,7 @@ function App() {
         <p>
           Is this so simple
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
