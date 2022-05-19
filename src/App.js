45297935cd319804391ed8a1388f7de953901795
rajeshvadlakonda <rajesh.vadlakonda101@gmail.com>
2022-05-19 18:17:47 +0530

    add

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Is this so simple
+          Is this so simple and easy
         </p>
        
       </header>
